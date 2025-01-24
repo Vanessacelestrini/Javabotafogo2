35718710ae4f12b3d2b6bf158b3ca7a5a155c91c
vanessa celestrini <vanessaxpc@gmail.com>
2025-01-23 21:33:49 -0300

    atualizando exercicios

---
+++ beta/src/controller/Controller.java
@@ -0,0 +1,25 @@
+package controller;
+
+import view.Tela;
+
+public class Controller {
+	public void verificarOpcao(int opcao) {
+		Tela tela = new Tela();
+		
+		if(opcao == 1) {
+			tela.exibirInclusao();
+			System.out.println("Inclusão de selecionada");
+		} else if(opcao == 2) {
+			System.out.println("Consulta selecionada");
+		} else if (opcao == 3) {
+			System.out.println("Alteração selecionada");
+		} else if(opcao == 4) {
+			System.out.println("Exclusão selecionada");
+		} else if(opcao == 0){
+			System.out.println("Aplicação encerrada");		
+		} else{
+			System.out.println("Opção digitada invalída");
+		
+		}		
+	}
+}
--- beta/src/model/AppTarefa.java
+++ beta/src/model/AppTarefa.java
@@ -2,39 +2,29 @@ package model;
 
 import java.util.Scanner;
 
+import controller.Controller;
+import view.Tela;
+
 public class AppTarefa {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+		Tela tela = new Tela();
+		Controller controla = new Controller();
+				
 		int opcao = 0;
 		
 		do {
-			System.out.println("----MENU PRINCIPAL-----");
-			System.out.println("1 - Inclusão de Tarefa");
-			System.out.println("2 - Consulta de Tarefa");
-			System.out.println("3 - Alteração de Tarefa");
-			System.out.println("4 - Exclusão de Tarefa");
-			System.out.println("0 - F I M ");
-			System.out.println("Digite a opção desejada:");
+			tela.exibirMenu();
+			
 			opcao = scanner.nextInt();
 			
-			if(opcao == 1) {
-				System.out.println("Inclusão de selecionada");
-			} else if(opcao ==2) {
-				System.out.println("Consulta selecionada");
-			} else if (opcao ==3) {
-				System.out.println("Alteração selecionada");
-			} else if(opcao == 4) {
-				System.out.println("Exclusão selecionada");
-			} else {
-				System.out.println("Aplicação encerrada");				
-			}
+			controla.verificarOpcao(opcao);
+			
 		} while (opcao !=0);
 	
          scanner.close();
 	}
 }
-
 
 
